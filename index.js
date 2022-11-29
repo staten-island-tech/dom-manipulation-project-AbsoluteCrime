9a6d2cae34689fc6ba32c5e90246d40683c96aa5
AbsoluteCrime <52426993+AbsoluteCrime@users.noreply.github.com>
2022-11-29 13:14:32 -0500

    3

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const DOMSelectors = {
-  button: document.getElementById("btn"),
+  buttonAdd: document.getElementById("add"),
+  buttonRmv: document.getElementById("rmv"),
   box: document.getElementById("container-box"),
   input: document.querySelector("#input"),
 };
 const cat = "meow";
 /* DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are an ${cat}</h1>`); */
-DOMSelectors.button.addEventListener("click", function () {
+DOMSelectors.buttonAdd.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
   DOMSelectors.input.value = "";
+});
+DOMSelectors.buttonRmv.addEventListener("click", function () {
+  DOMSelectors.box.remove();
 });
